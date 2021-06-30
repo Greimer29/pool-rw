a335bc6ab0192ca9c5203f620a1ecbf5fd3007ad
Greimer29 <greimerabelguzman29@gmail.com>
2021-06-29 21:04:17 -0400

    Ading new functionality: show persons in the pool

--- src/client/components/Pool.js
+++ src/client/components/Pool.js
@@ -1,9 +1,35 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react'
 import './pool.scss';
+import {Personas} from '../db.json';
 
 class Pool extends Component{
+    constructor(){
+        super();
+        this.state = {
+            Personas
+        }
+        this.Ver = this.Ver.bind(this);
+    }
+
+    Ver(e){
+        const aja = document.getElementById('aja');
+        if(aja.classList.contains('hide')){
+            aja.classList.remove('hide');
+        }else{
+            aja.classList.add('hide');
+        }
+    }
+
+
     render(){
+        const Person = this.state.Personas.map((persona,i) => {
+            return(
+                <div className="container  pool-pozo" key={i}>
+                    <h4>{persona.name}</h4>
+                </div>
+            )
+        })
         return(
             <div className="pool">
                 <div className="pool-clock"><h3>Timer</h3></div>
@@ -18,9 +44,12 @@ class Pool extends Component{
                         </div>
                     <button className="btn btn-warning bg-gradient my-2 w-100" onClick={this.timer}>Participar</button>
                         <a href="/como" className="link my-2">Cómo Funciona</a>
-                    <div className="pool-pozo">
+                    <div className="pool-pozo btn w-100" onClick={this.Ver}>
                         <div className="container">
                             Personas que entraron
+                            <div id="aja" className="container hide">
+                                {Person}
+                            </div>
                         </div>
                     </div>
                 </div>
